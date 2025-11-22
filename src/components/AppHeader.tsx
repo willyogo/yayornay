a0020ce7a71c6183fd776df46997d3e5b5e37544
willyogo <willy@ogorzaly.com>
2025-11-22 18:34:39 -0300

    Remove logo from header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -10,13 +10,6 @@ export function AppHeader({ view, onChange }: AppHeaderProps) {
   return (
     <header className="bg-white p-4">
       <div className="max-w-7xl mx-auto relative flex items-center justify-center">
-        <div className="absolute left-0 flex items-center gap-2">
-          <img
-            src="/.well-known/logo.png"
-            alt="YAYNAY Logo"
-            className="w-14 h-14 object-contain"
-          />
-        </div>
         <ViewToggle value={view} onChange={onChange} />
       </div>
     </header>
