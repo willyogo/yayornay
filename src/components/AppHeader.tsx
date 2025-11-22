92c43dd3b92438b04ec324ff27e59d2dead47134
willyogo <willy@ogorzaly.com>
2025-11-22 00:50:27 -0300

    Update AppHeader component

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -9,13 +9,13 @@ interface AppHeaderProps {
 
 export function AppHeader({ view, onChange }: AppHeaderProps) {
   return (
-    <header className="bg-white border-b border-gray-200 p-4">
+    <header className="bg-white p-4">
       <div className="max-w-7xl mx-auto relative flex items-center justify-center">
         <div className="absolute left-0 flex items-center gap-2">
           <img
             src="/.well-known/logo.png"
             alt="YAYNAY Logo"
-            className="w-10 h-10 object-contain"
+            className="w-14 h-14 object-contain"
           />
         </div>
         <ViewToggle value={view} onChange={onChange} />
